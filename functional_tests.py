47e96dbbd764872d6bf55c7e7f880bc5e9086423
shlomitArgov <shlomit.argov@gmail.com>
2015-02-22 22:20:29 +0200

    Added implicit wait in setup

--- functional_tests.py
+++ functional_tests.py
@@ -5,14 +5,15 @@ from selenium import webdriver
 class NewVisitorTest(unittest.TestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
 
     def tearDown(self):
         self.browser.quit()
 
     def test_starting_a_new_todo_list(self):
+        
         # Edith has heard about a cool new to-do lists  app.
         # She goes to its homepage
-
         self.browser.get('http://localhost:8000')
         self.assertIn('Django', self.browser.title)
 
