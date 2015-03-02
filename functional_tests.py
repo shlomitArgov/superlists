71bdc21fbb95df41233ac4b1bbdb97d29b2e9d89
shlomitArgov <shlomit.argov@gmail.com>
2015-03-02 20:46:33 +0200

    Functional test now checks we can input a to-do list item

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,7 @@
 __author__ = 'Shlomit'
 import unittest
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 class NewVisitorTest(unittest.TestCase):
     def setUp(self):
@@ -10,16 +11,43 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def test_starting_a_new_todo_list(self):
+    def test_can_start_a_list_and_retrieve_it_later(self):
         
         # Edith has heard about a cool new to-do lists  app.
         # She goes to its homepage
         self.browser.get('http://localhost:8000')
-        self.assertIn('Django', self.browser.title)
 
         # Edith notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
+        #She is invited to enter a to-do item straight away
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+
+        #She types in "Buy peacock feathers" into a text box (Edith's hobby
+        # is tying fly-fishing lures)\
+        inputbox.send_keys('Buy peacock feathers')
+
+        #When she hits enter, the page updates and now the page lists
+        # "1: Buy peacock feathers" as an item in a to-do list table
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_element_by_tag_name('br')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
+
+        #There is still a text box inviting her to add another item. She
+        #enters "Use peacock feathers to make a fly" (Edith is very methodical)
+        self.fail('Finish the test!')
+
+         # The page updates again, and now shows both items on her list
 
 
 
